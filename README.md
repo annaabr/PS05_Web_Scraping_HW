2e8f2755030f2d6ccfc9cdddb4cf86dbbba85a00
annaabr <annaabramyan2013@gmail.com>
2024-09-25 19:08:16 +0300

    Add spider

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PS05_Web_Scraping_HW
  Introduction to the Scrapy Library
+Написать spider для нахождения всех источников освещения с сайта divan.ru
+
+Нужно взять название источника освещения, цену и ссылку
---
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -0,0 +1,17 @@
+import scrapy
+
+
+class DivannewparsSpider(scrapy.Spider):
+    name = "divannewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/rostov-na-donu/category/svet"]
+
+    def parse(self, response):
+        lamps = response.css('div._Ud0k')
+        for lamp in lamps:
+            yield {
+                'name' : lamp.css('div.lsooF span::text').get(),
+                'price' : lamp.css('div.pY3d2 span::text').get(),
+                'url' : lamp.css('a').attrib['href']
+            }
+
